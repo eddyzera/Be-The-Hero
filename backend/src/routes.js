bad19cbe38e32dcb867d166f51ce16f59ae55630
eddyzera <nescau23041996@gmail.com>
2020-03-29 14:42:27 -0300

    Backend data validation with celebrate

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const OngController = require('./controller/ongController');
 const IncidentController = require('./controller/incidentController');
 const ProfileController = require('./controller/profilleController');
@@ -9,18 +11,91 @@ const routes = express.Router();
 //Session
 routes.post('/session', SessionController.create)
 
-// Ong
-routes.post('/ongs/create', OngController.create);
+/**
+ * ONGs
+ */
+routes.post('/ongs/create',
+
+    celebrate({
+
+        [Segments.BODY]: Joi.object().keys({
+
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required().min(10).max(11),
+            city: Joi.string().required(),
+            uf: Joi.string().required().length(2)
+
+        })
+
+    })
+, OngController.create);
+
 routes.get('/ongs', OngController.show);
 routes.put('/ongs/:ongId', OngController.update);
 routes.delete('/ongs/:ongId', OngController.destroy);
 
 //Incident
-routes.post('/ong/incident/create', IncidentController.create);
-routes.get('/ong/incident', IncidentController.index);
-routes.delete('/ong/incident/:incidentId', IncidentController.destroy);
+routes.post('/ong/incident/create',
+
+    celebrate({
+
+        [Segments.BODY]: Joi.object().keys({
+
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+            value: Joi.number().required()
+
+        }),
+
+        [Segments.HEADERS]: Joi.object().keys({
+
+            authorization: Joi.string().required()
+
+        }).unknown()
+    }) 
+, IncidentController.create);
+
+routes.get('/ong/incident', 
+
+    celebrate({
+
+        [Segments.QUERY]: Joi.object().keys({
+
+            page: Joi.number()
+
+        })
+
+    })
+
+, IncidentController.index);
+
+routes.delete('/ong/incident/:incidentId',
+
+    celebrate({
+
+        [Segments.PARAMS]: Joi.object().keys({
+
+            id: Joi.number().required()
+
+        })
+
+    }) 
+, IncidentController.destroy);
 
 //Profile
-routes.get('/ong/profile', ProfileController.index);
+routes.get('/ong/profile',
+
+    celebrate({
+
+        [Segments.HEADERS]: Joi.object().keys({
+
+            authorization: Joi.string().required()
+
+        }).unknown()
+
+    })
+
+, ProfileController.index);
 
 module.exports = routes;
